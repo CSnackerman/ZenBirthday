019cb7e83acb0523772ad1c66471ceea51a730a1
Colby <colbyackerman@gmail.com>
2021-10-28 22:59:09 +0000

    added candles

--- cake.js
+++ cake.js
@@ -10,8 +10,21 @@ class Cake {
         this.image.src = 'images/cake.png'
 
         this.candles = [
-                
+            new Candle (this.x + 50, this.y - 20),
+            new Candle (this.x + 61, this.y - 20),
+            new Candle (this.x + 72, this.y - 20),
+            new Candle (this.x + 83, this.y - 20),
+            new Candle (this.x + 94, this.y - 20),
+            new Candle (this.x + 105, this.y - 20),
+            new Candle (this.x + 116, this.y - 20),
+            new Candle (this.x + 127, this.y - 20),
+            new Candle (this.x + 138, this.y - 20),
+            new Candle (this.x + 149, this.y - 20),
+            new Candle (this.x + 160, this.y - 20),
+            new Candle (this.x + 171, this.y - 20),
+            new Candle (this.x + 171, this.y - 20),
         ]
+        
     }
 
     draw (context) {
